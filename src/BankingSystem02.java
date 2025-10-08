3f560f96d9405f0d7a9fbfbbbd82a12dfe9bdd7a
mushastudio <mrmusha2021@gmail.com>
2025-10-09 00:56:58 +0530

    add deposit ,withdraw featers

--- src/BankingSystem02.java
+++ src/BankingSystem02.java
@@ -14,19 +14,48 @@ public class BankingSystem02 {
         System.out.println("enter the account no:");
         int verifyAccountNoumber = scan.nextInt();
 
+        double deposit ;
+        double withdraw ;
+
+
         for (int i = 0; i < accountNumbers.length; i++) {
+            System.out.println("CHOOSE THE OPTION");
+            String choise = scan.next();
+
             if (accountNumbers[i] == verifyAccountNoumber) {
-                index = i;
-                break;
+
+
+                if (choise.equals("A")) {
+                    System.out.print("enter the deposit money :");
+                    deposit = scan.nextDouble();
+                    balances[i] += deposit;
+                    index = i;
+                    break;
+                }
+
+                if (choise.equals("B")) {
+                    System.out.print("enter the withdraw money :");
+                    withdraw = scan.nextDouble();
+                    if (withdraw <= balances[i]) {
+                        balances[i] -= withdraw;
+                        index = i;
+                        break;
+                    } else {
+                        System.out.println("in sufficient funds");
+
+                    }
+
+
+                }
 
             }
-        }
-        if (index!=-1){
-            System.out.println("enter the account no"+accountNumbers[index]);
-            System.out.println(accountHolders[index]+"balance :"+balances[index]);
 
         }
-        else {
+        if (index != -1) {
+
+            System.out.println(accountHolders[index] + " balance :" + balances[index]);
+
+        } else {
             System.out.println("invalid account number ");
         }
 
